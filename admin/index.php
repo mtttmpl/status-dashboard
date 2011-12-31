ac12c4a32d0ba6815f30db1ec3a64ff2da0a8c84
Matthew Temple <matt@mattytemple.com>
2011-12-31 22:02:27 +0000

    fixed admin include paths
    fixed include paths in the admin index.php for load and unload that
    would create an error

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,9 @@
 <?php
-require_once("includes/load.php");
+require_once("../includes/load.php");
 include("admin/theme/header.php");
 ?>
 
 <?php
 include("admin/theme/footer.php");
-require_once("includes/unload.php");
+require_once("../includes/unload.php");
 ?>
